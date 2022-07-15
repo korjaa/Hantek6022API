5309e5fb08f8c0634065e51cba25698f6b53972e
Martin <Ho-Ro@users.noreply.github.com>
2022-07-15 11:21:48 +0200

    bugfix for upload_*_firmware_from_hex.py
    Signed-off-by: Martin <Ho-Ro@users.noreply.github.com>

--- examples/upload_6021_firmware_from_hex.py
+++ examples/upload_6021_firmware_from_hex.py
@@ -5,8 +5,8 @@
 import sys
 from PyHT6022.LibUsbScope import Oscilloscope
 
-if len( argv ) > 1:
-    firmware = argv[ 1 ]
+if len( sys.argv ) > 1:
+    firmware = sys.argv[ 1 ]
     scope = Oscilloscope()
     scope.setup()
     if not scope.open_handle():
@@ -15,4 +15,4 @@ if len( argv ) > 1:
     print( "FW version", hex( scope.get_fw_version() ) )
     scope.close_handle()
 else:
-    print( "usage: " + argv[0] + " path_to_hexfile" )
+    print( "usage: " + sys.argv[0] + " path_to_hexfile" )
